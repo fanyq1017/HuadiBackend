9526c8042b810a85326eae2143bbc92154040af2
FYQ <fanyq1017@qq.com>
2022-07-12 14:50:37 +0800

    Initial commit

---
+++ src/main/java/com/example/huadibackend/HuadiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.huadibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuadiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuadiBackendApplication.class, args);
+    }
+
+}
