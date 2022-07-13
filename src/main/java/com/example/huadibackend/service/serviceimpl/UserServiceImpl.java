965ea5aabc0e837872d9417653399dba9b071d05
LKC <531528060@qq.com>
2022-07-13 14:40:13 +0800

    数据库操作的更新以及测试

---
+++ src/main/java/com/example/huadibackend/service/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.huadibackend.service.serviceimpl;
+
+import com.example.huadibackend.entity.User;
+import com.example.huadibackend.mapper.UserMapper;
+import com.example.huadibackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public User selectById(int userId) {
+        return userMapper.selectById(userId);
+    }
+
+    @Override
+    public void deleteById(int userId) {
+        userMapper.deleteById(userId);
+    }
+
+    @Override
+    public void insertUser(User user) {
+        userMapper.insertUser(user);
+    }
+
+    @Override
+    public void updateUserInformation(User user) {
+        userMapper.updateUserInformation(user);
+    }
+
+    @Override
+    public List<User> ShowUserInformation() {
+        return userMapper.ShowUserInformation();
+    }
+}
