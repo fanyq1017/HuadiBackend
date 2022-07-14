1846cfedae7f7b42a5a26dc83f251a17ffe929a7
vivion <1692557820@qq.com>
2022-07-14 15:13:54 +0800

    修改article表和建立vonluntaryproject表

--- src/main/resources/static/sql/huadi.sql
+++ src/main/resources/static/sql/huadi.sql
@@ -11,53 +11,43 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 13/07/2022 15:27:33
+ Date: 14/07/2022 15:11:36
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for activity
+-- Table structure for article
 -- ----------------------------
-DROP TABLE IF EXISTS `activity`;
-CREATE TABLE `activity`  (
-  `a_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '活动的唯一标识主键',
-  `a_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '活动小标题',
-  `a_image` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '活动的图片存放位置',
-  `a_information` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '活动信息',
-  PRIMARY KEY (`a_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'xx志愿者网的专题活动表（activity表）' ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+  `a_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '发布信息的唯一标识',
+  `a_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '发布文件的标题',
+  `u_id` int(0) NULL DEFAULT NULL COMMENT '发布作者的id',
+  `a_publishtime` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '第一次发布时间',
+  `a_mdContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '管理员发布的markdown文件的源码',
+  `a_state` int(0) NULL DEFAULT NULL COMMENT '0代表草稿箱，1代表已发表，2代表已删除',
+  `a_edittime` datetime(0) NULL DEFAULT NULL COMMENT '文章编辑时间\"类似最后一次修改的时间\"',
+  `a_type` int(0) NULL DEFAULT NULL COMMENT '0志愿活动1新闻2专题活动之类的',
+  `a_text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT 'html源码',
+  PRIMARY KEY (`a_id`) USING BTREE,
+  INDEX `u_id`(`u_id`) USING BTREE,
+  CONSTRAINT `u_id` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for help
 -- ----------------------------
 DROP TABLE IF EXISTS `help`;
 CREATE TABLE `help`  (
-  `h_id` int(0) NOT NULL COMMENT '帮扶表中help的唯一标识',
+  `h_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '帮扶表中help的唯一标识',
   `h_help` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '帮助其他人的人',
   `h_helped` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '被帮扶的人，就是发布求助需求的人',
   `h_type` int(0) NOT NULL COMMENT '0是帮扶关系未形成，1是帮扶关系已形成',
   `h_intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '帮扶信息',
   PRIMARY KEY (`h_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '雷锋热线中的帮扶表，只需打开列表查询帮扶相关信息即可' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for issue
--- ----------------------------
-DROP TABLE IF EXISTS `issue`;
-CREATE TABLE `issue`  (
-  `i_id` int(0) NOT NULL COMMENT '发布信息的唯一标识',
-  `i_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '发布文件的标题',
-  `u_id` int(0) NULL DEFAULT NULL COMMENT '发布作者的id',
-  `i_publishtime` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '第一次发布时间',
-  `i_mdContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '管理员发布的markdown文件的源码',
-  `i_state` int(0) NULL DEFAULT NULL COMMENT '0代表草稿箱，1代表已发表，2代表已删除',
-  `i_edittime` datetime(0) NULL DEFAULT NULL COMMENT '文章编辑时间\"类似最后一次修改的时间\"',
-  PRIMARY KEY (`i_id`) USING BTREE,
-  INDEX `u_id`(`u_id`) USING BTREE,
-  CONSTRAINT `u_id` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for recruit
@@ -90,12 +80,31 @@ CREATE TABLE `user`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video`  (
-  `v_id` int(0) NOT NULL COMMENT '唯一标识',
+  `v_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '唯一标识',
   `v_type` int(0) NOT NULL COMMENT '多媒体类型，类似0视频1图片2文本3网页',
   `v_location` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件路径',
   `v_intro` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '多媒体的介绍',
   `v_image` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '可以代表多媒体的图片',
   PRIMARY KEY (`v_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '多媒体资源库video表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for voluntaryproject
+-- ----------------------------
+DROP TABLE IF EXISTS `voluntaryproject`;
+CREATE TABLE `voluntaryproject`  (
+  `p_id` int(0) NOT NULL AUTO_INCREMENT COMMENT '项目编号',
+  `p_name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '项目名称',
+  `p_type` int(0) NULL DEFAULT NULL COMMENT '项目服务类别0应急救援之类的',
+  `p_location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '项目地点',
+  `p_publishDate` datetime(0) NULL DEFAULT NULL COMMENT '项目发布日期',
+  `p_projectStart` datetime(0) NULL DEFAULT NULL COMMENT '项目开始时间',
+  `p_projectEnd` datetime(0) NULL DEFAULT NULL COMMENT '项目结束时间',
+  `p_recruitStart` datetime(0) NULL DEFAULT NULL COMMENT '招募开始时间',
+  `p_recruitEnd` datetime(0) NULL DEFAULT NULL COMMENT '招募结束时间',
+  `p_serveclient` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '服务对象信息',
+  `p_projectInfo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '项目信息',
+  PRIMARY KEY (`p_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
